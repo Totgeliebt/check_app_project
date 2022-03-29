3524cc1c20545093c37ed2dd63d87398b8a5c4c4
zhuravlik <Zlaska@yandex.ru>
2022-03-29 14:36:46 +0300

    adding react telegram widget 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TelegramLoginButton from "react-telegram-login/src";
 import { App } from "./App";
+const handleTelegramResponse = response => {
+  console.log(response);
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="testBot" />,
+  document.getElementById('telegramButton')
+);
+// ReactDOM.render(<App />, document.getElementById("root"));
