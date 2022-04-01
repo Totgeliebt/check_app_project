cdaed3e51c87bc1022130a84553f77990cf44dc2
zhuravlik <Zlaska@yandex.ru>
2022-04-01 17:41:28 +0300

    post request fetch credentials added 3

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from "react";
 import TelegramLoginButton from "react-telegram-login";
 
 const Auth = () => {
-
   const handleTelegramResponse = (response) => {
     console.log("from teleg:", response);
     handleLogin(response);
-    console.log(response)
+    console.log(response);
   };
 
   const handleLogin = async (response) => {
-      const res = await fetch( "https://app-state.herokuapp.com/login", {
-        method: 'POST',
-        credentials: "same-origin",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json;charset=UTF-8'
-        },
-        body: JSON.stringify(response)
-      });
-      console.log(res);
-    };
+    const res = await fetch("https://app-state.herokuapp.com/login", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify(response),
+    });
+    console.log(res);
+  };
 
   return (
     <div>
