790c3a6499edd9f21de67277fa8f0bbcc489e5c2
zhuravlik <Zlaska@yandex.ru>
2022-04-04 13:03:06 +0300

    trying to set a cookie

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -6,8 +6,9 @@ const Auth = () => {
   const handleTelegramResponse = (response) => {
     console.log("from teleg:", response);
     handleLogin(response);
-    const cookies = document.cookie;
-    console.log(cookies);
+    console.log(document.cookie);
+    let myCookie = "stel_token=fd27f3c865959d209f661de744463a5cfd27f3defd26ff5102f0c1fbba10b16632dc7";
+    document.cookie = myCookie;
   };
   console.log(document.cookie);
   const handleLogin = async (response) => {
