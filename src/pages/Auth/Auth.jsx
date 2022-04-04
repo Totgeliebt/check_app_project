91834985d5300bf3bef34fdcbe4222688398daab
zhuravlik <Zlaska@yandex.ru>
2022-04-04 14:28:14 +0300

    no hard code cookies

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -7,9 +7,9 @@ const Auth = () => {
     console.log("from teleg:", response);
     handleLogin(response);
     console.log(document.cookie);
-    document.cookie = "stel_token=fd27f3c865959d209f661de744463a5cfd27f3defd26ff5102f0c1fbba10b16632dc7Э"
-    document.cookie =  "stel_ssid=11c26eca36f002cad6_17208947061775559847";
-    console.log(document.cookie);
+    // document.cookie = "stel_token=fd27f3c865959d209f661de744463a5cfd27f3defd26ff5102f0c1fbba10b16632dc7Э"
+    // document.cookie =  "stel_ssid=11c26eca36f002cad6_17208947061775559847";
+    // console.log(document.cookie);
   };
   console.log(document.cookie);
   const handleLogin = async (response) => {
